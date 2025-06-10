6e27804abde6b6d96534f462c005153d4128b5a4
SANDRA DWI ASTUTI <151292658+sandradwiastuti@users.noreply.github.com>
2025-06-10 09:04:06 +0700

    Update header.php

--- template/header.php
+++ template/header.php
@@ -4,9 +4,23 @@
   <meta charset="UTF-8">
   <title>EduTrack</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <style>
+    body {
+      background-color: transparent;
+    }
+
+    .navbar {
+      background-color: transparent !important;
+    }
+
+    .navbar .nav-link,
+    .navbar .navbar-brand {
+      color: white !important;
+    }
+  </style>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">EduTrack</a>
     <div class="collapse navbar-collapse">
@@ -17,4 +31,5 @@
     </div>
   </div>
 </nav>
+
 <div class="container mt-4">

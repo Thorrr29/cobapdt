f2e27053cda368eb127eda5df0f312668999693b
Ratu Berliana Rosfadila Machendra <151343648+brlilie@users.noreply.github.com>
2025-06-13 21:45:57 +0700

    Update functions.sql

--- sql/functions.sql
+++ sql/functions.sql
@@ -1,13 +1,13 @@
 DELIMITER //
 
-CREATE FUNCTION TotalPaid(uid INT) RETURNS DECIMAL(10,2)
+CREATE FUNCTION TotalPaidByCourse(uid INT, cid INT) RETURNS DECIMAL(10,2)
 DETERMINISTIC
 BEGIN
   DECLARE total DECIMAL(10,2);
   SELECT SUM(p.amount) INTO total
   FROM payments p
   JOIN enrollments e ON e.id = p.enrollment_id
-  WHERE e.user_id = uid;
+  WHERE e.user_id = uid AND e.course_id = cid;
   RETURN IFNULL(total, 0);
 END //
 
